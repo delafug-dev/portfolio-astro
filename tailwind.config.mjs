b40a9e9ed3141014a9025a0f4b457c35c1c875cd
delafug-dev <adelafuentegarceso@gmail.com>
2025-11-05 23:50:36 +0100

    Add social media section. Fix padding in Hero for smaller screens. Add font families and breakpoints to global CSS and Tailwind config.

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,13 @@
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,vue}"],
   darkMode: "class",
+  theme: {
+    extend: {
+      fontFamily: {
+        inter: ["Inter", "sans-serif"],
+        montserrat: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
   plugins: [require("tailwindcss-animated")],
 };
