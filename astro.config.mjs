3c998d7235239e7a0ca2f5d7f321224b87b481a9
delafug-dev <adelafuentegarceso@gmail.com>
2025-11-18 23:01:14 +0100

    ssr vercel para la api del clima

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,14 @@
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
